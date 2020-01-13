1c355b4755b070e45f680b341c59127b8e4e2ddf
YUKIKO IKEDA <applelele@gmail.com>
2020-01-13 00:31:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,6 +8,11 @@ def introduction(name)
 end
 introduction("Summer")
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+introduction_with_language("April", "Ruby")
+
 
 
 
